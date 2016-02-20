b62a44bf89f26704dd07648766670d34125dd79d
annapusai <annapusai573@gmail.com>
2016-02-19 20:29:54 -0600

    updated write using queue

--- src/test/NTest/main.cpp
+++ src/test/NTest/main.cpp
@@ -1,4 +1,4 @@
-#include <QCoreApplication>
+/*#include <QCoreApplication>
 #include <QFile>
 
 struct ipv4 {
@@ -20,10 +20,68 @@ int main(int argc, char *argv[])
 void writeBin()
 {
     ipv4 packet = {{192},{1},{1},{0}};
-    QFile f("W:/Test/ipv4_1.bin");
+    QFile f("E:/ipv4_1.bin");
 
     if (f.open(QFile::WriteOnly | QFile::Truncate)) {
        f.write(reinterpret_cast<char*>(&packet), sizeof(packet));
        f.close();
     }
+}
+*/
+
+
+
+
+#include <QCoreApplication>
+#include<QFile>
+#include<QString>
+#include<QDebug>
+#include<QTextStream>
+#include<QBuffer>
+#include<QQueue>
+
+
+void  Write(QString Filename) //writing into buffer
+{
+
+    QFile mfile(Filename);
+    if(!mfile.open(QFile::WriteOnly | QFile::Truncate))
+    {
+        qDebug() << "No Such File";
+        return;
+    }
+
+
+    QQueue queue;
+    queue.enqueue(mfile.readAll());
+
+
+    mfile.flush();
+    mfile.close();
+}
+/*void Read(QString Filename)
+{
+    QFile mfile(Filename);
+    if(!mfile.open(QFile::ReadOnly | QFile::Truncate))
+    {
+        qDebug() << "No Such File";
+        return;
+    }
+    QTextStream in(&mfile);
+    QString mText = in.readAll();
+
+    qDebug() << mText;
+
+    mfile.flush();
+    mfile.close();
+}
+ */
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QString mFilename = "E:/QT coding/Qt.bin"; //path of file
+    Write(mFilename);
+   // Read(mFilename);
+
+    return a.exec();
 }
